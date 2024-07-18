7a4f449302044161655be86bdbd04f9804e5dd05
yrii <glazyrin.yraf333@yandex.ru>
2024-07-18 19:20:10 +0300

    fixed styles

--- Front_react-ts/src/main.jsx
+++ Front_react-ts/src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './css/buttons.css';
-import './css/main_menu.css';
-import './css/modal.css';
 import './css/styles.css';
 
 
